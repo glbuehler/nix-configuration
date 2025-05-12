ebf9e0770ec94aa19878b548ef5d8d772cd949da
Gideon Bühler <gideonbuehler18@gmail.com>
2025-05-12 15:33:43 +0200

    update flake inputs

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -3,10 +3,9 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs } @ inputs: 
+  outputs = { self, nixpkgs }: 
   let 
     system = "x86_64-linux";
   in {
